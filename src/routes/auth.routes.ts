561cd35a05feedb13e95d385944dd4154c20c0f5
halit uzan <halit.uzan@gmail.com>
2024-10-28 21:45:27 +0300

    create profile image upload route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,10 +2,12 @@ const router = require("express").Router();
 
 import { CONFIG } from "../config";
 import { login, logOut, me, register } from "../middlewares/auth.middleware";
+import { uploadProfileImage } from "../middlewares/bucket.middleware";
 
 router.post(`${CONFIG.apiVersion}/login`, login);
 router.post(`${CONFIG.apiVersion}/register`, register);
 router.get(`${CONFIG.apiVersion}/logout`, logOut);
 router.get(`${CONFIG.apiVersion}/me`, me);
+router.post(`${CONFIG.apiVersion}/user/uploadProfileImage`, uploadProfileImage);
 
 export { router as authRoutes };
