8593be5a4f53fb219ff1827ad20b7fce4052ecef
halit uzan <halit.uzan@gmail.com>
2024-11-04 14:25:40 +0300

    follow service bug fix

--- src/middlewares/follow.middleware.ts
+++ src/middlewares/follow.middleware.ts
@@ -1,39 +1,77 @@
 import { Request, Response } from "express";
 import Follows from "../models/follow.model";
-
+import jwt from "jsonwebtoken";
+import tokenCheck from "../helpers/tokenCheck";
 //! Takibe alma fonksiyonu
 export const followUser = async (req: Request, res: Response) => {
-  const { userId, targetUserId } = req.body;
+  const { targetUserId } = req.body;
+  const token = tokenCheck(req, res) as any;
+  const secretKey = process.env.JWT_SECRET_KEY || "";
+
+  try {
+    jwt.verify(token, secretKey, async (err: any, decoded: any) => {
+      if (err) {
+        return res
+          .status(403)
+          .json({ status: false, message: "Geçersiz token" });
+      }
+
+      const userId = decoded?.userId;
+
+      if (!userId) {
+        return res.status(403).json({
+          status: false,
+          message: "Böyle bir kullanıcı mevcut değil.",
+        });
+      }
+      // Takip etmeye çalıştığı kişi kendisi ise error mesajı gönderiyoruz
+      if (targetUserId === userId) {
+        return res
+          .status(403)
+          .json({ status: false, message: "Kendinizi takip edemezsiniz." });
+      }
+
+      let follow = await Follows.findOne({
+        follower: userId,
+        following: targetUserId,
+      });
+
+      if (follow) {
+        // Eğer önceden bir takip ilişkisi varsa
+        // Mevcut ilişki kaldırılmışmı kontorlünü yapıyoruz.
+        if (!follow.isDeleted) {
+          return res
+            .status(400)
+            .json({
+              status: false,
+              message: "Bu kullanıcıyı takip ediyorsunuz.",
+            });
+        }
+        // Mevcutta ilişki varsa
+        // Kullanıcı takipten çıkarmışsa
+        // Kullanıcı tekrar takip etmek istiyorsa
+        // isDeleted ı kaldırıyoruz ve deleteAt i undefined yapıyoruz.
+        follow.isDeleted = false;
+        follow.deletedAt = undefined;
+        await follow.save();
+      } else {
+        // Eğer önceden bir takip ilişkisi yoksa yeni takip ilişkisi oluşturuyoruz.
+        follow = new Follows({ follower: userId, following: targetUserId });
+        await follow.save();
+      }
+
+      res
+        .status(200)
+        .json({ status: true, message: "Kullanıcı takibe alındı." });
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ status: false, message: "Sunucu hatası" });
+  }
+
   // TODO: userId takip etmek isteyen kullanıcı
   // TODO: targetUserId takip edilmek istenilen kullanıcı
   try {
-    let follow = await Follows.findOne({
-      follower: userId,
-      following: targetUserId,
-    });
-
-    if (follow) {
-      // Eğer önceden bir takip ilişkisi varsa
-      // Mevcut ilişki kaldırılmışmı kontorlünü yapıyoruz.
-      if (!follow.isDeleted) {
-        return res
-          .status(400)
-          .json({ message: "Bu kullanıcıyı takip etmiyorsunuz." });
-      }
-      // Mevcutta ilişki varsa
-      // Kullanıcı takipten çıkarmışsa
-      // Kullanıcı tekrar takip etmek istiyorsa
-      // isDeleted ı kaldırıyoruz ve deleteAt i undefined yapıyoruz.
-      follow.isDeleted = false;
-      follow.deletedAt = undefined;
-      await follow.save();
-    } else {
-      // Eğer önceden bir takip ilişkisi yoksa yeni takip ilişkisi oluşturuyoruz.
-      follow = new Follows({ follower: userId, following: targetUserId });
-      await follow.save();
-    }
-
-    res.status(200).json({ message: "Kullanıcı takibe alındı." });
   } catch (error) {
     res.status(500).json({ message: "Bir hata oluştu!", error });
   }
@@ -41,30 +79,50 @@ export const followUser = async (req: Request, res: Response) => {
 
 //! Takipten çıkarma fonksiyonu (soft delete)
 export const unfollowUser = async (req: Request, res: Response) => {
-  const { userId, targetUserId } = req.body;
-  // TODO: userId takipten çıkarmak isteyen kullanıcı
+  const { targetUserId } = req.body;
+  const token = tokenCheck(req, res) as any;
+  const secretKey = process.env.JWT_SECRET_KEY || "";
   // TODO: targetUserId takipten çıkarılmak istenen kullanıcı
+  jwt.verify(token, secretKey, async (err: any, decoded: any) => {
+    if (err)
+      return res.status(403).json({ status: false, message: "Geçersiz token" });
 
-  try {
-    const follow = await Follows.findOne({
-      follower: userId,
-      following: targetUserId,
-      isDeleted: false,
-    });
+    const userId = decoded?.userId;
 
-    if (!follow) {
-      return res
-        .status(404)
-        .json({ message: "Kullanıcı zaten takip ediliyor." });
+    if (!userId)
+      return res.status(403).json({
+        status: false,
+        message: "Böyle bir kullanıcı mevcut değil.",
+      });
+
+    // Takip etmeye çalıştığı kişi kendisi ise error mesajı gönderiyoruz
+    if (targetUserId === userId)
+      return res.status(403).json({
+        status: false,
+        message: "Kendinizi takipten çıkartamazsınız.",
+      });
+
+    try {
+      const follow = await Follows.findOne({
+        follower: userId,
+        following: targetUserId,
+        isDeleted: false,
+      });
+
+      if (!follow)
+        return res
+          .status(404)
+          .json({ message: "Kullanıcı zaten takip edilmiyor." });
+
+      // Soft delete işlemi
+      follow.isDeleted = true;
+      follow.deletedAt = new Date();
+
+      await follow.save();
+
+      res.status(200).json({ message: "Kullanıcı takipten çıkarıldı." });
+    } catch (error) {
+      res.status(500).json({ message: "Bir hata oluştu!", error });
     }
-    // Soft delete işlemi
-    follow.isDeleted = true;
-    follow.deletedAt = new Date();
-
-    await follow.save();
-
-    res.status(200).json({ message: "Kullanıcı takipten çıkarıldı." });
-  } catch (error) {
-    res.status(500).json({ message: "Bir hata oluştu!", error });
-  }
+  });
 };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,10 +2,10 @@ const router = require("express").Router();
 
 import { CONFIG } from "../config";
 import { uploadProfileImage } from "../middlewares/bucket.middleware";
-import { me, updateProfile } from "../middlewares/user.middleware";
+import { getUserProfile, me, updateProfile } from "../middlewares/user.middleware";
 router.get(`${CONFIG.version}/user/me`, me);
 router.patch(`${CONFIG.version}/user/uploadProfileImage`, uploadProfileImage);
 router.patch(`${CONFIG.version}/user/updateProfile`, updateProfile);
-router.get(`${CONFIG.version}/user/profile/:userName`,);
+router.get(`${CONFIG.version}/user/profile/:userName`,getUserProfile);
 
 export { router as userRoutes };
