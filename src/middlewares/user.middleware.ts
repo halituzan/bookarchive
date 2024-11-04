275c8eff01eedace5e44bc0214bc09a509a5b6be
halit uzan <halit.uzan@gmail.com>
2024-11-04 14:25:19 +0300

    user profile service opened public

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
-import Books from "../models/book.model";
-import Users from "../models/user.model";
 import tokenCheck from "../helpers/tokenCheck";
+import Users from "../models/user.model";
 import { UserProps } from "../types/global.types";
+import Follows from "../models/follow.model";
 
 export const updateProfile = async (
   req: Request,
@@ -94,13 +94,35 @@ export const me = async (req: Request, res: Response, next: () => void) => {
           .json({ message: "Böyle bir kullanıcı mevcut değil." });
       }
 
-      const user = await Users.findById(userId);
+      const user = await Users.findById(userId).select("-password -__v");
       if (!user) {
         return res.status(404).json({ message: "Kullanıcı bulunamadı." });
       }
 
       return res.json({ status: true, data: user });
     });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ status: false, message: "Sunucu hatası" });
+  }
+};
+
+export const getUserProfile = async (
+  req: Request,
+  res: Response,
+  next: () => void
+) => {
+  const { userName } = req.params;
+  try {
+    const user: any = await Users.find({ userName }).select("-password -__v");
+    if (user.length === 0) {
+      return res.status(404).json({ message: "Kullanıcı bulunamadı." });
+    }
+
+    const follows = await Follows.countDocuments({ follower: user[0]._id });
+    const followers = await Follows.countDocuments({ following: user[0]._id });
+
+    return res.json({ status: true, user: user[0], follows, followers });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ status: false, message: "Sunucu hatası" });
