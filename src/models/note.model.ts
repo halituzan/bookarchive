6a1c6471ef6086dc1c114141e5f065ad6e255091
halit uzan <halit.uzan@gmail.com>
2024-11-09 23:51:27 +0300

    added note service isPuclic field

--- src/models/note.model.ts
+++ src/models/note.model.ts
@@ -6,13 +6,19 @@ interface INotes extends refTypes {
   userBook: Schema.Types.ObjectId;
   note: string;
   notePage: number;
+  isPublic: number;
 }
+// isPublic
+// 0 = public
+// 1 = private
+// 2 = only friends
 const noteSchema = new Schema<INotes>(
   {
     user: { type: Schema.Types.ObjectId, ref: "Users", required: true },
     userBook: { type: Schema.Types.ObjectId, ref: "Books", required: true },
     note: { type: String, required: true, maxlength: 256 },
     isDeleted: { type: Boolean, default: false },
+    isPublic: { type: Number, default: 0 },
     notePage: { type: Number, default: 0 },
   },
   { timestamps: true }
